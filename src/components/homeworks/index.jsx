fec031cd350705ecc5928cf508c2d50ce4b44c56
Jaloliddin <bosimovjaloliddin@gmail.com>
2023-05-07 16:23:04 +0500

    homework state

---
+++ src/components/homeworks/index.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import Properties from "./properties"
+
+class Homeworks extends React.Component{
+    render(){
+        return <>
+           <Properties/>
+        </>
+    }
+}
+export default Homeworks
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Test from "./components/testing";
+import Homeworks from "./components/homeworks";
+// import Test from "./components/testing";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Test />
+    <Homeworks />
+    {/* <Test /> */}
   </React.StrictMode>
 );
