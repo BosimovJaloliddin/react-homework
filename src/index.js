9feaaa27ecce200a827945379118a69f62172214
Jaloliddin <bosimovjaloliddin@gmail.com>
2023-05-16 00:08:09 +0500

    houzing

--- src/index.js
+++ src/index.js
@@ -3,15 +3,17 @@ import ReactDOM from "react-dom/client";
 // import Homeworks from "./components/homeworks/homeworks";
 import "./style.css";
 // import Test from "./components/testing";
-import Crud from "./components/crud/crud";
-import Table from "./components/table/table";
+// import Crud from "./components/crud/crud";
+// import Table from "./components/table/table";
+import Styled from "./components/styled/styled";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <Homeworks /> */}
     {/* <Test /> */}
-    <Crud />
-    <Table />
+    {/* <Crud /> */}
+    {/* <Table /> */}
+    <Styled />
   </React.StrictMode>
 );
