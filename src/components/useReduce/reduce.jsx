fa5d07fed268755669caa255589b5356fce8afd2
Jaloliddin <bosimovjaloliddin@gmail.com>
2023-05-20 11:40:40 +0500

    useReducer homework

--- src/components/useReduce/reduce.jsx
+++ src/components/useReduce/reduce.jsx
@@ -1,45 +1,58 @@
 import React, { useReducer } from "react";
+import { Container, Title, Wrapper, Box, BoxBtn, Button } from "./styled";
 
 const Reduce = () => {
-  const baza = {
-    count: 0,
-    type: "1",
-  };
   const reduce = (state, action) => {
-    console.log(state, action);
     switch (action.type) {
       case "1":
-        return { ...action, count: action.count + 1 };
+        return { ...state, count: state.count + action.payload };
       case "5":
-        return { ...action, count: action.count + 5 };
+        return { ...state, count: state.count + action.payload };
       case "20":
-        return { ...action, count: action.count + 20 };
+        return { ...state, count: state.count + action.payload };
       case "50":
-        return { ...action, count: action.count + 50 };
+        return { ...state, count: state.count + action.payload };
       case "100":
-        return { ...action, count: action.count + 100 };
+        return { ...state, count: state.count + action.payload };
       default:
-        return { ...action };
+        return { ...state };
     }
   };
+  const [data, dispatch] = useReducer(reduce, { count: 0, type: "1" });
   const onSelect = ({ target: { value } }) => {
-    dispatch({ ...baza, type: (baza.type = value) });
+    data.type = value;
   };
-  const [data, dispatch] = useReducer(reduce, baza);
-  console.log(baza.count);
   return (
-    <div>
-      <h1>Counter:{data.count}</h1>
-      <select onChange={onSelect} name="select" id="select">
-        <option value="1">1</option>
-        <option value="5">5</option>
-        <option value="20">20</option>
-        <option value="50">50</option>
-        <option value="100">100</option>
-      </select>
-      <button onClick={() => dispatch(data)}>{data.type}</button>
-      <button onClick={() => dispatch(data)}>{data.type}</button>
-    </div>
+    <Container>
+      <Wrapper>
+        <Title>
+          Counter:<span>{data.count}</span>
+        </Title>
+        <Box>
+          <select onChange={onSelect} name="select" id="select">
+            <option value="1">1</option>
+            <option value="5">5</option>
+            <option value="20">20</option>
+            <option value="50">50</option>
+            <option value="100">100</option>
+          </select>
+          <BoxBtn>
+            <Button
+              cl="black"
+              onClick={() => dispatch({ type: data.type, payload: +data.type })}
+            >
+              +{data.type}
+            </Button>
+            <Button
+              cl="red"
+              onClick={() => dispatch({ type: data.type, payload: -data.type })}
+            >
+              -{data.type}
+            </Button>
+          </BoxBtn>
+        </Box>
+      </Wrapper>
+    </Container>
   );
 };
 
---
+++ src/components/useReduce/styled.jsx
@@ -0,0 +1,72 @@
+import { styled } from "styled-components";
+
+export const Container = styled.div`
+  max-width: 1200px;
+  margin: 4rem;
+`;
+export const Wrapper = styled.h1`
+  max-width: 400px;
+  text-align: center;
+  margin: 0 auto;
+  border: 2px solid #ddd;
+  border-radius: 7px;
+  box-shadow: 0 0 7px #222;
+  padding: 2rem;
+`;
+export const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 800;
+  color: black;
+  letter-spacing: 3px;
+  span {
+    color: #000d83;
+  }
+`;
+export const Box = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+  select {
+    font-size: 18px;
+    font-weight: 700;
+    width: 80px;
+    height: 30px;
+    border: 2px solid #ddd;
+    border-radius: 7px;
+    text-align: center;
+    color: green;
+    &:hover {
+      font-size: 24px;
+      transform: scale(1.2, 1.1);
+      transition: all 1s;
+    }
+  }
+`;
+const getColor = (props) => {
+  return {
+    black: "#fff",
+    red: "red",
+  }[props.cl];
+};
+export const BoxBtn = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 20px;
+`;
+export const Button = styled.button`
+  width: 80px;
+  height: 30px;
+  font-size: 18px;
+  font-weight: 800;
+  color: ${getColor};
+  background: #4141f0;
+  border: none;
+  border-radius: 7px;
+  &:hover {
+    box-shadow: 0 0 7px #4141f0;
+    transition: all 0.4s;
+    font-size: 20px;
+  }
+`;
